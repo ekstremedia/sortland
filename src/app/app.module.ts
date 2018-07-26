ce5378c1e4a17bf4407bd74f2ed74c5a117a9808
Ekstremedia <terje@ekstremedia.no>
2018-07-26 21:46:37 +0200

    Fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
 import { AppRoutingModule } from './/app-routing.module';
@@ -8,9 +7,6 @@ import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 
 // from https://angular.io/guide/router
-const appRoutes: Routes = [
-  { path: 'welcome', component: WelcomeComponent }
-];
 
 
 @NgModule({
@@ -20,7 +16,6 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
     UiModule,
     AppRoutingModule
   ],
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from '../pages/welcome/welcome.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent }
+];
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(appRoutes),
   ],
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   exports: [LayoutComponent]
