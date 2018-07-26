393bce7d76fc29d771096fd885d2ffa8b69910e7
Ekstremedia <terje@ekstremedia.no>
2018-07-26 10:51:47 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { AppRoutingModule } from './/app-routing.module';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+
+
+// from https://angular.io/guide/router
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    UiModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
